0dd0087e048d59f059552b4aae7c767d1d691955
bram <en4abraham@gmail.com>
2025-02-16 09:10:49 -0800

    Modified smart contract

--- week-4-projects/Abraham-Enoma/tuesday-task/smart-contract/ignition/modules/VotingContract.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("VoteModule", (m) => {
-  const votingContract = m.contract("Vote", []);
-
-  return { votingContract };
-});
