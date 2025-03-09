3c63a7fc70a55c8aa61c638dbdb04033bb20ceb1
Obada Akivie <greydot.42@protonmail.com>
2025-03-09 18:39:20 +0100

    Add week 3 & 4 projects for obada

--- week-5-projects/obadaakivie/smart contract/solidity/ignition/modules/GrantContract.js
+++ week-5-projects/obadaakivie/smart contract/solidity/ignition/modules/GrantContract.js
@@ -6,13 +6,12 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI = 1_000_000_000n;
 
-module.exports = buildModule("LockModule", (m) => {
+module.exports = buildModule("GrantContractModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const GrantContract = m.contract("GrantContract"
+  );
 
-  return { lock };
+  return { GrantContract };
 });
